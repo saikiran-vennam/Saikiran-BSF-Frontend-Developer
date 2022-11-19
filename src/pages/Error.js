6f890dca300927a7b0fcc52bd11c1c21d16217bf
saikiran-vennam <saikiranvennam64@gmail.com>
2022-11-19 20:00:46 +0530

    Error Component Adjusting

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -6,7 +6,7 @@ export default function Error() {
       <section className="flex flex-col items-center justify-center text-center h-screen">
         <h1>Error | The requested resource could not be found</h1>
         <Link to="/" className="btn">
-          &larr; Back to Homepage
+          &larr; 
         </Link>
       </section>
     </>
